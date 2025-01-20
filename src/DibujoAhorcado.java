8bfcf6d18e63237f490d66f3b6910c3a55b891a4
dam1 - Búa Alonso <A22AnxoBA@sanclemente.local>
2025-01-20 10:49:57 +0100

    Version 0.1

---
+++ src/DibujoAhorcado.java
@@ -0,0 +1,136 @@
+class DibujoAhorcado{
+    static void dibujar(int i) {
+
+        switch (i) {
+            case 1:
+                System.out.println(" ---------------------");
+                for (int j = 0; j< 15; j++) {
+                    System.out.println(" |");
+
+                }
+                System.out.println("__________");
+                break;
+
+            case 2:
+                System.out.println(" ---------------------");
+                System.out.println(" |                     |");
+                System.out.println(" |                     |");
+                System.out.println(" |                  -------");
+                System.out.println(" |                 | -  -  |");
+                System.out.println(" |                 |   o   |");
+                System.out.println(" |                  -------");
+                for (int j = 0; j< 10; j++) {
+                    System.out.println(" |");
+
+                }
+                System.out.println("__________");
+                break;
+
+            case 3:
+                System.out.println(" ---------------------");
+                System.out.println(" |                     |");
+                System.out.println(" |                     |");
+                System.out.println(" |                  -------");
+                System.out.println(" |                 | -  -  |");
+                System.out.println(" |                 |   o   |");
+                System.out.println(" |                  -------");
+                System.out.println(" |                     |   ");
+                System.out.println(" |                     |   ");
+                System.out.println(" |                     |   ");
+                System.out.println(" |                     |   ");
+                System.out.println(" |                     |   ");
+                for (int j = 0; j< 5; j++) {
+                    System.out.println(" |");
+
+                }
+                System.out.println("__________");
+                break;
+
+            case 4:
+                System.out.println(" ---------------------");
+                System.out.println(" |                     |");
+                System.out.println(" |                     |");
+                System.out.println(" |                  -------");
+                System.out.println(" |                 | -  -  |");
+                System.out.println(" |                 |   o   |");
+                System.out.println(" |                  -------");
+                System.out.println(" |                     |   ");
+                System.out.println(" |                   / |   ");
+                System.out.println(" |                 /   |   ");
+                System.out.println(" |                /    |   ");
+                System.out.println(" |                     |   ");
+                for (int j = 0; j< 5; j++) {
+                    System.out.println(" |");
+
+                }
+                System.out.println("__________");
+                break;
+
+            case 5:
+                System.out.println(" ---------------------");
+                System.out.println(" |                     |");
+                System.out.println(" |                     |");
+                System.out.println(" |                  -------");
+                System.out.println(" |                 | -  -  |");
+                System.out.println(" |                 |   o   |");
+                System.out.println(" |                  -------");
+                System.out.println(" |                     |   ");
+                System.out.println(" |                   / | \\ ");
+                System.out.println(" |                  /  |   \\ ");
+                System.out.println(" |                 /   |     \\ ");
+                System.out.println(" |                     |   ");
+                for (int j = 0; j< 5; j++) {
+                    System.out.println(" |");
+
+                }
+                System.out.println("__________");
+                break;
+
+            case 6:
+                System.out.println(" ---------------------");
+                System.out.println(" |                     |");
+                System.out.println(" |                     |");
+                System.out.println(" |                  -------");
+                System.out.println(" |                 | -  -  |");
+                System.out.println(" |                 |   o   |");
+                System.out.println(" |                  -------");
+                System.out.println(" |                     |   ");
+                System.out.println(" |                   / | \\ ");
+                System.out.println(" |                  /  |   \\ ");
+                System.out.println(" |                 /   |     \\ ");
+                System.out.println(" |                     |   ");
+                System.out.println(" |                    /  ");
+                System.out.println(" |                   /      ");
+                System.out.println(" |                  /       ");
+                for (int j = 0; j< 2; j++) {
+                    System.out.println(" |");
+
+                }
+                System.out.println("__________");
+                break;
+
+            case 7:
+                System.out.println(" ---------------------");
+                System.out.println(" |                     |");
+                System.out.println(" |                     |");
+                System.out.println(" |                  -------");
+                System.out.println(" |                 | X  X  |");
+                System.out.println(" |                 |   o   |");
+                System.out.println(" |                  -------");
+                System.out.println(" |                     |   ");
+                System.out.println(" |                   / | \\ ");
+                System.out.println(" |                  /  |   \\ ");
+                System.out.println(" |                 /   |     \\ ");
+                System.out.println(" |                     |   ");
+                System.out.println(" |                    / \\");
+                System.out.println(" |                   /   \\  ");
+                System.out.println(" |                  /     \\ ");
+                for (int j = 0; j< 2; j++) {
+                    System.out.println(" |");
+
+                }
+                System.out.println("__________");
+                break;
+        }
+    }
+}
---
+++ src/Main.kt
@@ -0,0 +1,39 @@
+fun main(){
+    val animales = listOf("Perro", "Gato", "Conejo", "Oso", "Tigre", "Jirafa", "Elefante", "Mono", "Cebra")
+    println("Bienvenido al juego del ahorcado: ")
+    val palabra = animales.random().lowercase()
+    val estadoPalabra = MutableList(palabra.length) { '*' }
+    var intentos = 7
+    println("Adivina el animal. Tienes $intentos intentos")
+    println(estadoPalabra.joinToString(""))
+
+    while (intentos>0){
+        println("Introduce una letra: ")
+        val letra = readln()
+
+        if (palabra.contains(letra)){
+            println("Correcto! La letra $letra está en la palabra")
+            for (i in palabra.indices) {
+                if (palabra[i].toString() == letra) {
+                    estadoPalabra[i] = letra[0]
+                }
+            }
+        } else{
+            println("La letra $letra no está en la palabra")
+            intentos--
+            if (intentos==0) {
+                println("Has agotado todos tus intentos")
+                break
+            }
+        }
+
+
+
+        println("Adivina el animal. Tienes $intentos intentos")
+        println(estadoPalabra.joinToString(""))
+
+        if (!estadoPalabra.contains('*')){
+            println("Felicidades! Has acertado la palabra")
+        }
+    }
+}
