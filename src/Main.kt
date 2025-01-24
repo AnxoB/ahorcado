67b41c08cb5eb69371215c93ca8345c803d4785f
Anxo Bua Alonso <a22anxoba@iessanclemente.net>
2025-01-24 19:18:08 +0100

    Version 1.0

--- src/Main.kt
+++ src/Main.kt
@@ -4,7 +4,7 @@ fun main(){
     val animales = listOf("Perro", "Gato", "Conejo", "Oso", "Tigre", "Jirafa", "Elefante", "Mono", "Cebra")
     println("Bienvenido al juego del ahorcado: ")
     val palabra = animales.random().lowercase()
-    val estadoPalabra = MutableList(palabra.length) { '*' }
+    val estadoPalabra = MutableList(palabra.length){'*'}
     var intentos = 0
     println("Adivina el animal. Te quedan 7 intentos")
     println(estadoPalabra.joinToString(""))
@@ -13,6 +13,10 @@ fun main(){
         println("Introduce una letra: ")
         val letra = readln()
 
+        if (letra.length > 1 || letra.isEmpty()){
+            println("El valor introducido es incorrecto")
+            continue
+        }
         if (letra in palabra){
             println("Correcto! La letra $letra está en la palabra")
             for (i in palabra.indices) {
@@ -22,7 +26,7 @@ fun main(){
             }
         } else{
             intentos++
-            var dibujo=DibujoAhorcado.dibujar(intentos)
+            val dibujo=DibujoAhorcado.dibujar(intentos)
             println(dibujo)
             println("La letra $letra no está en la palabra")
             if (intentos==7) {
@@ -31,15 +35,13 @@ fun main(){
             }
         }
 
-
-
-        println("Adivina el animal. Tienes " + (7-intentos) + " intentos")
-        println(estadoPalabra.joinToString(""))
-
         if (('*') !in estadoPalabra){
             println("Felicidades! Has acertado la palabra")
             break
         }
+
+        println("Adivina el animal. Tienes " + (7-intentos) + " intentos")
+        println(estadoPalabra.joinToString(""))
     }
     reproductor.cerrar()
 }
